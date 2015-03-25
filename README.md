9eeb9e2f18217bb95d7f382ffe066da055feaf40
Ian Thomas <Ian.Thomas@bskyb.com>
2015-03-25 10:56:25 +0000

    working on the readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # UX Designer Technical Test
 
+## Problem Overview
+
+* Use CSS to style the provided markup to create a clean, intuitive user experience.
+* Fix any HTML that you may consider doesn't follow current best-practise guidelines
+* Embellish the user experience using animation, interactions and updated process flows
+
+## What We're Looking For
+
+* Strong visual design
+* Well implemented typography and vertical rhythm
+* Clean CSS
+* Clear user journey and interaction patterns
+* Understanding of JavaScript implementation and ability to update where necessary
+
+## Requirements
+
+* The latest version of Chrome or Firefox
+
+## Problem Details
+
+This test is built on top of our demo API ([http://skybettechtestapi.herokuapp.com]()) which is used to pull in a list of available bets and submit stakes to place actual bets.
+
+## Notes
+
+Ideally, we want to see vanilla solutions so would prefer that existing frameworks (such as Bootstrap or Foundation) are not used. This does not include pre-processors, which can be used to enhance the writing of CSS if desired.
+
+## Extra Credit
+
+* Include brand elements to follow the design direction seen on [skybet.com]() and [m.skybet.com]()
+* Include media queries to provide optimal experiences for devices of varying capabilities
+* Enhance the JavaScript to give better feedback and introduce extra interaction touches
+* Update the markup & CSS to introduce flexbox where appropriate
--- js/main.js
+++ js/main.js
@@ -1,19 +1,84 @@
+function renderBets($container, $template, bets) {
+    $container.html('');
+    bets.forEach(function (bet) {
+        var $bet = $template.clone();
+        $bet.data(bet);
+        $bet.find('.bet__event').text(bet.event);
+        $bet.find('.bet__name').text(bet.name);
+        $bet.find('.bet__odds').text(formatOdds(bet.odds)).data('odds', bet.odds);
+
+        $container.append($bet);
+    });
+}
+
+function renderSelection($container, $selection) {
+    var id = 'selection-' + $selection.data('bet_id');
+    var sel = $selection.data();
+    $selection.attr('id', id);
+    $selection.find('.selection__title').text(
+        sel.event + ' - ' + sel.name
+    );
+    $selection.find('.selection__returns').text('@ ' +
+                                                formatOdds(sel.odds) +
+                                                ' could return £' +
+                                                formatReturns(sel.odds, 0));
+
+    $container.append($selection);
+}
+
+function renderReceipt($container, $receipt) {
+    var data = $receipt.data();
+    //this would be a nice use case for variable destructuring in ES6
+    var id = 'receipt-' + data.bet_id;
+    $receipt.attr('id', id);
+
+    $receipt.find('.receipt__title').text(
+        data.event + ' - ' + data.name
+    );
+    $receipt.find('.receipt__stake').text('£' +
+                                          data.stake +
+                                          ' @ ' +
+                                          formatOdds(data.odds) +
+                                          ' could return £' +
+                                          formatReturns(data.odds, data.stake));
+    $receipt.find('.receipt__transaction-id').text(data.transaction_id);
+    $container.append($receipt);
+}
+
+function removeSelection(selectionId) {
+    $('#selection-' + selectionId).remove();
+}
+
+function formatOdds(odds) {
+    return odds.numerator === odds.denominator ? 'Evens' : odds.numerator + '/' + odds.denominator;
+}
+
+function formatReturns(odds, stake) {
+    var nil = 0;
+    return stake > 0 ? ((1 + (odds.numerator / odds.denominator)) * stake).toFixed(2) : nil.toFixed(2);
+}
+
+function betLoadError(error) {
+    console.error(error);
+}
+
 var templates = ["bet", "selection", "receipt"];
 var $templates = {};
 var betslip = [];
+var $bets = $('#bets'), $selections = $('#selections'), $receipts = $('#receipts');
+var $app = $('body');
 
 templates.forEach(function (template) {
     $templates[template] = $($('#' + template).html());
 });
 
-var $bets = $('#bets'), $selections = $('#selections'), $receipts = $('#receipts');
-
 $.get('http://skybettechtestapi.herokuapp.com/available')
     .done(renderBets.bind(this, $bets, $templates['bet']))
     .fail(betLoadError);
 
-var $app = $('body');
-
+/**
+ * @todo Think about how the betslip show behave when selections are added and removed
+ */
 $app.on('click', '.js-odds', function () {
     var $bet = $(this).parent('.bet');
     var betId = $bet.data('bet_id');
@@ -35,9 +100,13 @@ $app.on('submit', '.js-selection', function (e) {
     var $selection = $(this);
 
     var stake = $selection.find('.js-stake').val();
+    var $button = $selection.find('.js-place-bet');
 
     if (stake > 0) {
         var data = $.extend($selection.data(), {stake: stake});
+
+        $button.attr('disabled', true);
+
         $.ajax({
             url: 'http://skybettechtestapi.herokuapp.com/bets',
             method: 'post',
@@ -47,14 +116,13 @@ $app.on('submit', '.js-selection', function (e) {
         }).done(
             function (data) {
                 //remove bet from selections and add to the receipts
-                $selection.remove();
-
+                removeSelection(data.bet_id);
                 var $receipt = $templates['receipt'].clone();
                 $receipt.data(data);
                 renderReceipt($receipts, $receipt);
-
             }
         ).fail(function (e) {
+                //@todo show error message returned from the server
                 $selection.addClass('is-invalid');
                 console.error(e);
             }
@@ -67,78 +135,20 @@ $app.on('submit', '.js-selection', function (e) {
 $app.on('keyup', '.js-stake', function () {
     var $selection = $(this).closest('.js-selection');
     var sel = $selection.data();
+
+    var stake = parseFloat($(this).val());
+    if (stake > 0) {
+        $selection.find('.js-place-bet').attr('disabled', false);
+    } else {
+        $selection.find('.js-place-bet').attr('disabled', true);
+    }
     $selection.find('.js-returns').html('@ ' +
                                         formatOdds(sel.odds) +
                                         ' could return £' +
-                                        formatReturns(sel.odds, $(this).val()));
+                                        formatReturns(sel.odds, stake));
 });
 
 $app.on('click', '.js-toggle', function (e) {
     e.preventDefault();
     $(this).parent().find('.js-toggle-target').toggleClass('is-hidden');
 });
-
-
-function renderBets($container, $template, bets) {
-    $container.html('');
-    bets.forEach(function (bet) {
-        var $bet = $template.clone();
-        $bet.data(bet);
-        $bet.find('.bet__event').html(bet.event);
-        $bet.find('.bet__name').html(bet.name);
-        $bet.find('.bet__odds').html(formatOdds(bet.odds)).data('odds', bet.odds);
-
-        $container.append($bet);
-    });
-}
-
-function renderSelection($container, $selection) {
-    var id = 'selection-' + $selection.data('bet_id');
-    var sel = $selection.data();
-    $selection.attr('id', id);
-    $selection.find('.selection__title').html(
-        sel.event + ' - ' + sel.name
-    );
-    $selection.find('.selection__returns').html('@ ' +
-                                                formatOdds(sel.odds) +
-                                                ' could return £' +
-                                                formatReturns(sel.odds, 0));
-
-    $container.append($selection);
-}
-
-function renderReceipt($container, $receipt) {
-    var data = $receipt.data();
-    //this would be a nice use case for variable destructuring in ES6
-    var id = 'receipt-' + data.bet_id;
-    $receipt.attr('id', id);
-
-    $receipt.find('.receipt__title').html(
-        data.event + ' - ' + data.name
-    );
-    $receipt.find('.receipt__stake').html('£' +
-                                          data.stake +
-                                          ' @ ' +
-                                          formatOdds(data.odds) +
-                                          ' could return £' +
-                                          formatReturns(data.odds, data.stake));
-    $receipt.find('.receipt__transaction-id').html(data.transaction_id);
-    $container.append($receipt);
-}
-
-function removeSelection(selectionId) {
-    $('#selection-' + selectionId).remove();
-}
-
-function formatOdds(odds) {
-    return odds.numerator === odds.denominator ? 'Evens' : odds.numerator + '/' + odds.denominator;
-}
-
-function formatReturns(odds, stake) {
-    var nil = 0;
-    return stake > 0 ? ((1 + (odds.numerator / odds.denominator)) * stake).toFixed(2) : nil.toFixed(2);
-}
-
-function betLoadError(error) {
-    console.error(error);
-}
